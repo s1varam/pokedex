04f5bdea79ea04fe9139af3ec871c8ceb6cf97cc
sivaramkumar_ <sivaramkumar925@gmail.com>
2021-06-15 22:29:00 +0530

    Revert "code cleanup"
    This reverts commit d8bddeb7fdc04153e5f02827e7e27640c28bad8d.

--- src/App.js
+++ src/App.js
@@ -77,13 +77,13 @@ class App extends React.Component {
             types: [
                 "all types", "grass", "bug", "dark", "dragon", "electric", "fairy", "fighting", "fire", "flying", "ghost", "ground", "ice", "normal", "poison", "psychic", "rock", "steel", "water"
             ]
+
         }
-        this.getAllPokemons();
     }
 
-    // componentWillMount() {
-    //     this.getAllPokemons();
-    // }
+    componentWillMount() {
+        this.getAllPokemons();
+    }
 
     componentDidMount() {
         var currentTheme = document.documentElement.getAttribute('data-theme');
@@ -96,7 +96,9 @@ class App extends React.Component {
 
     getAllPokemons = async () => {
         debugger
+
         const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${this.state.limit}&offset=${this.state.offset}`).catch((err) => console.log("Error:", err));
+
         this.getPokemonData(response.data.results);
 
     }
@@ -109,27 +111,28 @@ class App extends React.Component {
             this.state.allPokemons.push(response.data)
         }
 
-        // this.state.showLoading = false;
+        this.state.showLoading = false;
+
         this.setState({
             allPokemons: this.state.allPokemons,
-            showLoading : false,
 
         })
 
-        // console.log(this.state.allPokemons);
+        console.log(this.state.allPokemons);
 
     }
 
     fetchPokemonData = async (number, pokemon, category, imageURL) => {
 
         debugger
+
         this.setState({
             abilities: [],
             stats: []
         })
 
         const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`).catch((err) => console.log("Error:", err));
-        // console.log(response);
+        console.log(response);
 
         for (var i = 0; i < response.data.abilities.length; i++) {
             this.state.abilities.push(response.data.abilities[i].ability.name);
@@ -153,8 +156,8 @@ class App extends React.Component {
             showInfo: true,
         })
 
-        // console.log("stats");
-        // console.log(this.state.stats);
+        console.log("stats");
+        console.log(this.state.stats);
 
         this.fetchPokemonDescription(pokemon);
 
@@ -162,6 +165,7 @@ class App extends React.Component {
 
     fetchPokemonDescription = async (pokemon_name) => {
         debugger
+        var desc;
 
         try {
             const response = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${pokemon_name}`).catch((err) => console.log("Error:", err));
@@ -182,8 +186,8 @@ class App extends React.Component {
             })
         }
 
-        // console.log("description");
-        // console.log(desc);
+        console.log("description");
+        console.log(desc);
     }
 
     closeDialog = () => {
@@ -195,10 +199,8 @@ class App extends React.Component {
     handleChangeRegions = (event) => {
 
         debugger
-        // this.state.isFilter = false;
-        this.setState({
-            isFilter: false
-        })
+
+        this.state.isFilter = false;
 
         for (var i = 0; i < this.state.regions.length; i++) {
             if (this.state.regions[i].name === event.target.value) {
@@ -217,11 +219,23 @@ class App extends React.Component {
             }
         }
 
+        // this.state.limit = region.limit;
+        // this.state.offset = region.offset;
+        // this.state.allPokemons = [];
+        // this.state.showLoading = true;
+
+        // this.setState({
+        //     allPokemons : [],
+        //     limit : region.limit,
+        //     offset: region.offset,
+        // })
+
         this.forceUpdate();
 
-        // console.log("limit");
-        // console.log(event.target.value);
-
+        console.log("limit");
+        console.log(event.target.value);
+        // console.log("offset");
+        // console.log(this.state.offset)
 
         this.getAllPokemons();
     }
@@ -229,6 +243,7 @@ class App extends React.Component {
     handleChangeTypes = (event) => {
 
         debugger
+
         if (event.target.value === "all types") {
             this.setState({
                 isFilter: false,
@@ -237,6 +252,7 @@ class App extends React.Component {
             return;
         }
 
+        // this.state.swapPokemons = this.state.allPokemons;
         this.state.isSearch = false;
         this.state.valuesearch = "";
         this.state.isFilter = true;
@@ -256,6 +272,7 @@ class App extends React.Component {
             valuetype: event.target.value,
         })
 
+        // this.state.allPokemons = this.state.filterPokemons;
         this.forceUpdate();
 
     }
@@ -276,8 +293,8 @@ class App extends React.Component {
 
         this.state.searchPokemons.length === 0 ? this.setState({ noDataFound: true }) : this.setState({ noDataFound: false })
 
-        // console.log("search array");
-        // console.log(this.state.searchPokemons);
+        console.log("search array");
+        console.log(this.state.searchPokemons);
 
     }
 
@@ -289,9 +306,10 @@ class App extends React.Component {
 
         debugger
         var currentTheme = document.documentElement.getAttribute('data-theme');
-        // console.log(currentTheme);
+        console.log(currentTheme);
 
         var targetTheme = "light";
+        var modeSwitchText = "Light"
 
         if (currentTheme === "light") {
             targetTheme = "dark";
@@ -300,12 +318,15 @@ class App extends React.Component {
                 isChecked: true,
             })
 
-            // console.log(targetTheme);
+            console.log(targetTheme);
         } else {
             this.setState({
                 isChecked: false,
             })
         }
+
+        // var modeSwitch = document.getElementById("mode__label");
+        // modeSwitch.innerText = modeSwitchText;
 
         document.documentElement.setAttribute('data-theme', targetTheme)
 
@@ -320,7 +341,7 @@ class App extends React.Component {
                             Loading
                         </div>
                         <div className="gif__container">
-                            <img src="https://i.gifer.com/VgI.gif" className="loading__gif noselect" alt="loading-gif"></img>
+                            <img src="https://i.gifer.com/VgI.gif" className="loading__gif noselect"></img>
                         </div>
                     </div>}
                 {!this.state.showLoading && <div className="app__container">
@@ -338,8 +359,13 @@ class App extends React.Component {
                             description={this.state.description}
                             cancel={() => this.closeDialog()}>
                         </InfoDialog>}
+                    {/* <Header
+                        className="container__header"
+                        regions={this.state.regions}
+                    /> */}
                     <div className="app__header">
                         <div className="switch">
+
                             <div className="toggle">
                                 <label for="themeSwitch"></label>
                                 <input type="checkbox" name="swich-theme" id="themeSwitch" onClick={this.changeTheme} checked={this.state.isChecked} />
@@ -366,6 +392,7 @@ class App extends React.Component {
                                 {this.state.regions.map((region) => (
                                     <option value={region.name}>{region.name}&nbsp;({region.offset + 1}-{region.limit + region.offset})</option>
                                 ))}
+
                             </select>
                         </div>
                         <div className="filter__items">
@@ -396,6 +423,9 @@ class App extends React.Component {
                                     type={this.state.searchPokemons[item].types}
                                     onElemClick={() => this.fetchPokemonData(this.state.searchPokemons[item].id, this.state.searchPokemons[item].name, this.state.searchPokemons[item].types, this.state.searchPokemons[item].sprites.other.dream_world.front_default ? this.state.searchPokemons[item].sprites.other.dream_world.front_default : this.state.searchPokemons[item].sprites.other['official-artwork'].front_default)}
                                 />) :
+
+
+
                                 (!this.state.isFilter ? Object.keys(this.state.allPokemons).map((item, index) =>
                                     <Pokemon
                                         key={index}
@@ -418,6 +448,7 @@ class App extends React.Component {
                                     ))
                             }
                         </div>
+                        {/* <button className="load__more" onClick={() => this.getAllPokemons()}>Load More</button> */}
                     </div>
 
                     {this.state.noDataFound && <div className="no__data noselect">
@@ -432,8 +463,11 @@ class App extends React.Component {
                             <GitHubIcon></GitHubIcon>
                         </div>
                     </div>
+
+
                 </div>}
             </>
+
         )
     }
 }
--- src/components/InfoDialog.js
+++ src/components/InfoDialog.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Dialog from '@material-ui/core/Dialog';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
 import MuiDialogContent from '@material-ui/core/DialogContent';
+import MuiDialogActions from '@material-ui/core/DialogActions';
 import { StylesProvider } from "@material-ui/core/styles";
 import './InfoDialog.css';
-import { colorTypeGradients } from '../utils';
+
+
+const styles = (theme) => ({
+    root: {
+        margin: 0,
+        padding: theme.spacing(2),
+    },
+    closeButton: {
+        display: 'none',
+        position: 'absolute',
+        right: theme.spacing(1),
+        top: theme.spacing(1),
+        color: theme.palette.grey[500],
+    },
+});
+
+// const DialogTitle = withStyles(styles)((props) => {
+//     const { children, open, classes, onClose, ...other } = props;
+//     return (
+//         <MuiDialogTitle disableTypography className={classes.root} {...other}>
+//             <Typography variant="h6">{children}</Typography>
+//             {onClose ? (
+//                 <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
+//                     <CloseIcon />
+//                 </IconButton>
+//             ) : null}
+//         </MuiDialogTitle>
+//     );
+// });
 
 const DialogContent = withStyles((theme) => ({
     root: {
@@ -12,23 +42,163 @@ const DialogContent = withStyles((theme) => ({
     },
 }))(MuiDialogContent);
 
+const DialogActions = withStyles((theme) => ({
+    root: {
+        margin: 0,
+        padding: theme.spacing(1),
+    },
+}))(MuiDialogActions);
+
 export default function InfoDialog(props) {
+    const [open, setOpen] = React.useState(false);
 
-    var finalColor;
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
 
-    if(props.category.length === 2){
-        finalColor = colorTypeGradients(props.category[0].type.name,props.category[1].type.name,props.category.length);
-    }else{
-        finalColor = colorTypeGradients(props.category[0].type.name,props.category[0].type.name,props.category.length);
+    var color1, color2;
+
+
+    switch (props.category[0].type.name) {
+        case "grass":
+            color1 = "rgba(155, 204, 80,0.65)";
+            break;
+        case "poison":
+            color1 = "rgba(185, 127, 201,0.6)";
+            break;
+        case "normal":
+            color1 = "#dcdcdc";
+            break;
+        case "fire":
+            color1 = "rgba(253, 125, 36, 0.4)";
+            break;
+        case "water":
+            color1 = "rgba(69, 146, 196, 0.5)";
+            break;
+        case "electric":
+            color1 = "rgba(239, 214, 54, 0.75)";
+            break;
+        case "ice":
+            color1 = "#51c4e7";
+            break;
+        case "fighting":
+            color1 = "rgba(213, 103, 35, 0.65)";
+            break;
+        case "ground":
+            color1 = "rgba(247, 222, 63,0.5)";
+            break;
+        case "flying":
+            color1 = "rgba(61, 199, 239, 0.65)";
+            break;
+        case "psychic":
+            color1 = "rgba(243, 102, 185, 0.65)";
+            break;
+        case "bug":
+            color1 = "rgba(114, 159, 63, 0.6)";
+            break;
+        case "rock":
+            color1 = "rgba(163, 140, 33, 0.6)";
+            break;
+        case "ghost":
+            color1 = "rgba(123, 98, 163, 0.5)";
+            break;
+        case "dark":
+            color1 = "rgba(112, 112, 112, 0.6)";
+            break;
+        case "dragon":
+            color1 = "#f26f58";
+            break;
+        case "steel":
+            color1 = "#9fb8b9";
+            break;
+        case "fairy":
+            color1 = "#fdb9e9";
+            break;
+        default:
+        // code block
     }
 
-   
+    if (props.category.length == 2) {
+
+        switch (props.category[1].type.name) {
+            case "grass":
+                color2 = "rgba(155, 204, 80,0.65)";
+                break;
+            case "poison":
+                color2 = "rgba(185, 127, 201,0.6)";
+                break;
+            case "normal":
+                color2 = "#dcdcdc";
+                break;
+            case "fire":
+                color2 = "rgba(253, 125, 36, 0.4)";
+                break;
+            case "water":
+                color2 = "rgba(69, 146, 196, 0.5)";
+                break;
+            case "electric":
+                color2 = "rgba(239, 214, 54, 0.75)";
+                break;
+            case "ice":
+                color2 = "#51c4e7";
+                break;
+            case "fighting":
+                color2 = "rgba(213, 103, 35, 0.65)";
+                break;
+            case "ground":
+                color2 = "rgba(247, 222, 63,0.5)";
+                break;
+            case "flying":
+                color2 = "rgba(61, 199, 239, 0.65)";
+                break;
+            case "psychic":
+                color2 = "rgba(243, 102, 185, 0.65)";
+                break;
+            case "bug":
+                color2 = "rgba(114, 159, 63, 0.6)";
+                break;
+            case "rock":
+                color2 = "rgba(163, 140, 33, 0.6)";
+                break;
+            case "ghost":
+                color2 = "rgba(123, 98, 163, 0.5)";
+                break;
+            case "dark":
+                color2 = "rgba(112, 112, 112, 0.6)";
+                break;
+            case "dragon":
+                color2 = "#f26f58";
+                break;
+            case "steel":
+                color2 = "#9fb8b9";
+                break;
+            case "fairy":
+                color2 = "#fdb9e9";
+                break;
+            default:
+            // code block
+        }
+    } else if (props.category.length == 1) {
+        color2 = color1;
+    }
+
+    let closeImg = { cursor: 'pointer', float: 'right', marginTop: '5px', width: '20px' };
+
     return (
         <>
             <StylesProvider injectFirst>
                 <div>
+
+                    {/* <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+        Open dialog
+      </Button> */}
+      
                     <Dialog aria-labelledby="customized-dialog-title" open={props.open} onBackdropClick={props.cancel} fullWidth maxWidth="md" className="dialog__bg noselect">
-                        <DialogContent style={{ background: `linear-gradient(${finalColor[0]}, ${finalColor[1]})` }} className="dialog__content">
+                        {/* <img src='https://d30y9cdsu7xlg0.cloudfront.net/png/53504-200.png' onClick={props.cancel} style={closeImg} /> */}
+                        <DialogContent style={{ background: `linear-gradient(${color1}, ${color2})` }} className="dialog__content">
                             <div className="info__container">
                                 <div className="info__container__img">
                                     <div className="pokemon__id">
@@ -38,12 +208,13 @@ export default function InfoDialog(props) {
                                         {props.name}
                                     </div>
                                     <div>
-                                        <img src={props.img} alt="pokemon-img" />
+                                        <img src={props.img} />
                                     </div>
                                     <div className="info__container__data__type">
+                                        {/* <p className="info__container__headings">Type</p> */}
                                         {props.category.map((category) =>
                                             <div className={`poke__type__bg ${category.type.name}`}>
-                                                <img src={`types/${category.type.name}.png`} title={category.type.name} alt="poketype-img"></img>
+                                                <img src={`${category.type.name}.png`} title={category.type.name}></img>
                                             </div>
                                         )}
                                     </div>
@@ -54,6 +225,7 @@ export default function InfoDialog(props) {
                                 </div>
 
                                 <div className="info__container__data">
+                                    {/* <div><img src='https://d30y9cdsu7xlg0.cloudfront.net/png/53504-200.png' className="close__btn" onClick={props.cancel} style={closeImg} /></div> */}
                                     <div className="right__box">
                                         <div>
                                             {props.description}
@@ -68,6 +240,8 @@ export default function InfoDialog(props) {
                                                 </div>
                                             </div>
                                         </div>
+
+
                                         <div>
                                             <div className="info__container__headings stats">Stats</div>
                                             <div className="info__container__data__data">
