439ca9502d79815519d0f6c1172ff900711fe41a
sivaramkumar_ <sivaramkumar925@gmail.com>
2021-06-23 12:35:47 +0530

    css changes for mobile

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -33,6 +33,7 @@ const Pokemon = ({ id, image, name, type, onElemClick }) => {
                     visibleByDefault={false}
                     delayMethod={'debounce'}
                     effect="blur"
+                    className="img__thumbnail"
                 />
             </div>
             <div className="poke__name" >
