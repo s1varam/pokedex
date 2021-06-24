f1c0997b0069e021dd052e41322f9ace67f06242
sivaramkumar_ <sivaramkumar925@gmail.com>
2021-06-24 22:05:53 +0530

    Code refactor - footer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Pokedex from "../src/assets/images/pokedex.png";
 import InfoDialog from "./components/InfoDialog";
 import axios from 'axios';
 import GitHubIcon from '@material-ui/icons/GitHub';
-import Scroll from './components/Scroll'
+import Scroll from './components/Scroll';
+import Footer from './components/Footer'
 
 class App extends React.Component {
 
@@ -487,14 +488,15 @@ class App extends React.Component {
                     {this.state.noDataFound && <div className="no__data noselect">
                         No such Pokémon in this region :/
                     </div>}
-                    <div className="app__footer noselect">
+                    {/* <div className="app__footer noselect">
                         <div>
                             Built using <a href="https://pokeapi.co/" target="_blank" rel="noopener noreferrer">PokéAPI</a>, <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React</a> and <a href="https://material-ui.com/" target="_blank" rel="noopener noreferrer">Material-UI</a>.
                         </div>
                         <div onClick={this.openGithub} className="github__icon">
                             <GitHubIcon></GitHubIcon>
                         </div>
-                    </div>
+                    </div> */}
+                    <Footer/>
                 </div>}
             </>
         )
---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import GitHubIcon from '@material-ui/icons/GitHub';
+
+class Footer extends React.Component {
+
+    openGithub = () => {
+        window.open("https://github.com/s1varam/pokedex");
+    }
+
+    render() {
+        return (
+            <>
+               <div className="app__footer noselect">
+                        <div>
+                            Built using <a href="https://pokeapi.co/" target="_blank" rel="noopener noreferrer">PokéAPI</a>, <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React</a> and <a href="https://material-ui.com/" target="_blank" rel="noopener noreferrer">Material-UI</a>.
+                        </div>
+                        <div onClick={this.openGithub} className="github__icon">
+                            <GitHubIcon></GitHubIcon>
+                        </div>
+                    </div>
+            </>
+        )
+    }
+}
+
+export default Footer;
